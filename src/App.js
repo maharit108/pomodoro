3227c6673d41f6b9f14b2e9264d2bf79afa93ac0
maharit108 <maha.rit108@gmail.com>
2021-01-18 10:50:17 -0600

    1/18

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         delayInMinutes: workTime, periodInMinutes: workTime
       })
       chrome.alarms.create('workStart', {
-        delayInMinutes: (workTime+breakTime), periodInMinutes: (workTime+breakTime)
+        delayInMinutes: (workTime + breakTime), periodInMinutes: (workTime + breakTime)
       })
     }
     setNeedAlarm(false)
@@ -31,8 +31,8 @@ function App() {
     <div className="App">
       <h1>Pomodoro</h1>
       <p>An effective Way of working</p>
-      <button onClick={setWorkTime(1)}>1</button>
-      <button onClick={setBreakTime(1)}>2</button>
+      <button onClick={() => setWorkTime(1)}>1</button>
+      <button onClick={() => setBreakTime(1)}>2</button>
       <p>Work for {workTime} minutes, then take break for {breakTime} minutes</p>
       <button id='App__toggleAlarm' onClick={createAlarm} >Create Alarm</button>
       <button id='App__toggleAlarm' onClick={cancelAlarm} >Remove Alarm</button>
