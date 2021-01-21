4f4f53e394e5c959a19442fed0714d01925be895
maharit108 <maha.rit108@gmail.com>
2021-01-21 16:07:58 -0600

    Modal component created. trying without using alarm

--- public/app/background.js
+++ public/app/background.js
@@ -1,5 +1,11 @@
 /*global chrome*/
+let work = true
 
-chrome.alarms.onAlarm.addListener(function( alarm ) {
-    alert('hi, alarm fired')
-})
+  chrome.alarms.onAlarm.addListener(alarm => {
+    alert("Break Time!")
+    chrome.notifications.create(message);
+    //communicate to content.js:
+    chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, {message: "alarm"});
+    });
+  });
--- src/App.js
+++ src/App.js
@@ -3,27 +3,24 @@
 import React, { useState } from 'react'
 import './App.css';
 
+import ModalAlert from './ModalAlert.jsx'
+
 function App() {
 
-  const [needAlarm, setNeedAlarm ] = useState(true)
   const [workTime, setWorkTime] = useState(0.05)
   const [breakTime, setBreakTime] = useState(0.05)
   
   const createAlarm = () => {
-    if (needAlarm) {
-      chrome.alarms.create('workEnd', {
-        delayInMinutes: workTime, periodInMinutes: workTime
-      })
-      chrome.alarms.create('workStart', {
-        delayInMinutes: (workTime + breakTime), periodInMinutes: (workTime + breakTime)
-      })
-    }
-    setNeedAlarm(false)
+    chrome.alarms.create('breakStart', {
+      delayInMinutes: workTime
+    })
+    window.close()
   }
 
   const cancelAlarm = () => {
     chrome.alarms.clearAll(() => {
-      alert('Work Session ended')
+      <ModalAlert title='Work End' body='All Alarms Removed. Pomodoro Work Sessions End.' />
+      window.close()
     })
   }
 
@@ -31,8 +28,17 @@ function App() {
     <div className="App">
       <h1>Pomodoro</h1>
       <p>An effective Way of working</p>
-      <button onClick={() => setWorkTime(1)}>1</button>
-      <button onClick={() => setBreakTime(1)}>2</button>
+      <h2>Work Session Duration</h2>
+      <button onClick={setWorkTime(2)}>2 Mins</button>
+      <button onClick={setWorkTime(25)}>25 Mins</button>
+      <button onClick={setWorkTime(30)}>30 Mins</button>
+      <button onClick={setWorkTime(35)}>35 Mins</button>
+      <button onClick={setWorkTime(40)}>40 Mins</button>
+      <h2>Break Duration</h2>
+      <button onClick={setBreakTime(1)}>1 Mins</button>
+      <button onClick={setBreakTime(5)}>5 Mins</button>
+      <button onClick={setBreakTime(10)}>10 Mins</button>
+      <button onClick={setBreakTime(15)}>15 Mins</button>
       <p>Work for {workTime} minutes, then take break for {breakTime} minutes</p>
       <button id='App__toggleAlarm' onClick={createAlarm} >Create Alarm</button>
       <button id='App__toggleAlarm' onClick={cancelAlarm} >Remove Alarm</button>
