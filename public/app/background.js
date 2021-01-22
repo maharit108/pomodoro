5056f6f91040fe789d34096624f802e5784f5174
maharit108 <maha.rit108@gmail.com>
2021-01-22 12:51:28 -0600

    Add audio file to play on alert

--- public/app/background.js
+++ public/app/background.js
@@ -1,11 +1,13 @@
 /*global chrome*/
+  let audioPlay = new Audio(chrome.runtime.getURL('./../audio.mp3'))
 
   chrome.alarms.onAlarm.addListener(alarm => {
-    console.log(alarm, alarm.name)
     if (alarm.name === 'breakStart') {
+      audioPlay.play()
       alert('---Break Time!!!--- \n Take a break!!! \n \n To End Alarms, Click on the Pomodoro Extension and Remove Alarm')
     }
     if (alarm.name === 'workStart') {
+      audioPlay.play()
       alert('---Work Time!!!--- \n Time to go back to Work!!!  \n \n To End Alarms, Click on the Pomodoro Extension and Remove Alarm')
     }
   });
